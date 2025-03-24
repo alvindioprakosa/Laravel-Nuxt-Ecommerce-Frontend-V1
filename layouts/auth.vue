959d50e31285e9d2526bb7273ff19187501eb2fa
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-24 21:35:25 +0700

    Update Layout Auth

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,36 +1,28 @@
 <template>
-  <div class="c-app flex-row align-items-center">
+  <div class="c-app d-flex flex-row align-items-center">
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-md-4">
-
-            <Nuxt />
-
+          <Nuxt />
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-  export default {
+<style scoped>
+/* Pastikan font hanya diterapkan di komponen ini jika tidak digunakan di global */
+body {
+  font-family: "Quicksand", sans-serif;
+  font-size: initial !important;
+}
 
+.form-control {
+  height: auto;
+  font-size: 1rem;
+}
 
-  }
-</script>
-
-<style>
-  body {
-    font-family: 'Quicksand', sans-serif;
-    font-size: initial!important;
-  }
-
-  .form-control {
-    height: initial;
-    font-size: 1rem;
-  }
-
-  .c-app {
-      background-color: #BCCAD8!important;
-  }
+.c-app {
+  background-color: #bccad8 !important;
+}
 </style>
