50e00de186da6991da4be23574ca4884a9326eff
alvindioprakosa <alvindioprakosa@gmail.com>
2025-04-12 10:40:17 +0700

    Update Slider

--- components/web/slider.vue
+++ components/web/slider.vue
@@ -1,50 +1,74 @@
 <template>
-  <div class="container-fluid mt-custom">
+  <div class="container-fluid mt-4">
     <div id="mycarousel" class="carousel slide" data-ride="carousel" data-interval="4000">
-      
       <div class="carousel-inner">
-
-        <div class="carousel-item" v-for="(slider, id) in sliders" :class="{ active: id==0 }" :key='slider.id'>
-            <a :href="slider.link" target="_blank">
-                <img :src="slider.image" class="d-block w-100 rounded">
-            </a> 
+        <div
+          class="carousel-item"
+          v-for="(slider, id) in sliders"
+          :class="{ active: id === 0 }"
+          :key="slider.id"
+        >
+          <a :href="slider.link" target="_blank" rel="noopener noreferrer">
+            <img
+              :src="slider.image || defaultImage"
+              class="d-block w-100 rounded"
+              alt="Slider Image"
+            />
+          </a>
         </div>
-
-      </div> 
+      </div>
 
       <a class="carousel-control-prev" href="#mycarousel" role="button" data-slide="prev">
-        <div class="banner-icons"> <span class="fa fa-angle-left"></span> </div> <span class="sr-only">Previous</span>
-      </a> 
+        <div class="banner-icons">
+          <span class="fa fa-angle-left"></span>
+        </div>
+        <span class="sr-only">Previous</span>
+      </a>
       <a class="carousel-control-next" href="#mycarousel" role="button" data-slide="next">
-        <div class="banner-icons"> <span class="fa fa-angle-right"></span> </div> <span class="sr-only">Next</span>
+        <div class="banner-icons">
+          <span class="fa fa-angle-right"></span>
+        </div>
+        <span class="sr-only">Next</span>
       </a>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
+export default {
+  data() {
+    return {
+      defaultImage: '/images/default-slider.jpg', // path fallback image jika tidak ada slider.image
+    };
+  },
 
-    //hook "fetch"
-    async fetch() {
+  async fetch() {
+    // Ambil data slider dari Vuex store
+    await this.$store.dispatch('web/slider/getSlidersData');
+  },
 
-      //fething sliders on Rest API
-      await this.$store.dispatch('web/slider/getSlidersData')
-
+  computed: {
+    sliders() {
+      return this.$store.state.web.slider.sliders;
     },
-
-    //computed
-    computed: {
-
-      //sliders
-      sliders() {
-        return this.$store.state.web.slider.sliders
-      }
-    }
-
-  }
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.carousel-item img {
+  max-height: 500px;
+  object-fit: cover;
+}
 
+.banner-icons {
+  background-color: rgba(0, 0, 0, 0.4);
+  padding: 10px;
+  border-radius: 50%;
+  color: white;
+  font-size: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
